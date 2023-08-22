5424febb21d64e560a71dff86363fdd70934b6e9
rerribe <rerribe@pop-os.localdomain>
2023-08-22 14:08:56 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,75 @@
+//jshint eversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    // console.log(firstName, lastName, email)
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/e35685ea11";
+
+    const options = {
+        method: "POST",
+        auth: "algum:eff19d9c8056c3f34124b14e7161adaf-us21"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server rodando na porta 3000");
+});
+
+
+// chave API mailchimp
+// eff19d9c8056c3f34124b14e7161adaf-us21
+
+// audience ID
+// e35685ea11
